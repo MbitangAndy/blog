60a2409962d062d5343093cacbbb0da203aee41d
mbitangandy <mbitangangel.com>
2024-08-20 13:44:52 +0100

    first modification

---
+++ blog/urls.py
@@ -0,0 +1,15 @@
+
+from django.contrib import admin
+from django.urls import path, include
+from .views import *
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', home, name="home"),
+    path('contact',contact, name="contact-us"),
+    path('posts/', include('posts.urls')),
+    path('accounts/', include('users.urls'), name='accounts')
+]
+
+
+# CRUD
